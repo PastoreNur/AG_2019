edea093d313cbd9b9dd5df1330ae102e3876e763
Julio Pastore <julio.pastore.jp@gmail.com>
2019-06-30 01:29:22 -0600

    Inicio
    Inicio

---
+++ cmake-build-debug/CMakeFiles/camara_basica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camara_basica.dir/main.cpp.o"
+  "camara_basica.pdb"
+  "camara_basica"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camara_basica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/camara_basica.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for camara_basica.
+# This may be replaced when dependencies are built.
